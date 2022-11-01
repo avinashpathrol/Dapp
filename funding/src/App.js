8e822267c327300e7286cae06324a3e28797cb8d
AvinashPathrol <35654094+avinashpathrol@users.noreply.github.com>
2022-10-31 21:54:53 -0600

    🇫🇮 final

--- funding/src/App.js
+++ funding/src/App.js
@@ -2,26 +2,33 @@ import { useState,useEffect } from 'react';
 import Web3 from 'web3';
 import './App.css';
 import detectEthereumProvider from '@metamask/detect-provider';
-
+import {loadContract} from '../src/utils/load-Contract'
 function App() {
 
   const[web3Api,setWeb3Api] = useState({
     provider:null,
-    web3:null
+    web3:null,
+    contract:null
   })
 
   const[account,setAccount] = useState(null);
+  const [balance,setBalance] = useState(null);
+  const [reload,shouldReload] = useState(false);
+
+  const reloadEffect = () => shouldReload(!reload);
   useEffect(() => {
     const loadProvider = async() => {
       console.log(window.web3)
       console.log(window.ethereum)
       // let provider = null;
       const provider = await detectEthereumProvider();
+      const contract = await loadContract("Funder",provider);
       if(provider){
         provider.request({method:"eth_requestAccounts"})
         setWeb3Api({
           web3:new Web3(provider),
-          provider
+          provider,
+          contract
         })
       }
       else {
@@ -50,6 +57,35 @@ function App() {
     loadProvider();
   },[]);
 
+
+
+  useEffect(()=>{
+    const loadBalance = async() =>{
+      const {contract,web3} = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance,"ether"));
+    };
+    web3Api.contract && loadBalance()
+  },[web3Api,reload])
+
+const transferFund = async()=>{
+  const {web3,contract} = web3Api;
+  await contract.transfer({
+    from:account,
+    value:web3.utils.toWei("2","ether")
+  });
+  reloadEffect();
+};
+
+const withdrawFund = async() =>{
+  const{contract,web3} = web3Api;
+  const withdrawAmount = web3.utils.toWei("2","ether");
+  await contract.withdraw(withdrawAmount,{
+    from:account,
+  });
+  reloadEffect();
+};
+
   useEffect(()=>{
     const getAccounts = async() =>{
       const accounts = await web3Api.web3.eth.getAccounts()
@@ -63,7 +99,7 @@ function App() {
     <div class="card text-center">
       <div class="card-header">Funding</div>
       <div class="card-body">
-        <h5 class="card-title">Balance:  ETH </h5>
+        <h5 class="card-title">Balance: {balance} ETH </h5>
         <p class="card-text">
           Account : {account ? account:"Not Connected" }
         </p>
@@ -80,11 +116,11 @@ function App() {
           Connect to metamask
         </button> */}
         &nbsp;
-        <button type="button" class="btn btn-success " >
+        <button type="button" class="btn btn-success " onClick={transferFund} >
           Transfer
         </button>
         &nbsp;
-        <button type="button" class="btn btn-primary " >
+        <button type="button" class="btn btn-primary " onClick={withdrawFund} >
           Withdraw
         </button>
       </div>
